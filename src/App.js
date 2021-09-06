9f79d3812c234ffca01f8675c709479d4da227de
github username <cmyerskeitt@gmail.com>
2021-09-06 13:09:47 -0400

    setup and test each page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import './App.css'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Reviews from './Pages/Reviews'
 
 function App() {
   return (
@@ -9,8 +13,20 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact/>
+          <Route exact path='/' >
+            <Home />
+          </Route>
+          <Route path='/about' >
+            <About/>
+          </Route>
+          <Route exact path='/contact' >
+            <Contact />
+          </Route>
+          <Route exact path='/reviews' >
+            <Reviews />
+          </Route>
         </Switch>
+
       </Router>
      
     </>
---
+++ src/Pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+class Home extends React.Component{
+  
+ render(){
+     return (
+       
+        <div>Welcome to Diamond Notary!</div> 
+    
+        )
+    }
+}
+
+
+
+export default Home 
