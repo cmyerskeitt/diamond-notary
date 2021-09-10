f9d3e679c9eb9def83f7f445cc6979b6723aaac1
github username <cmyerskeitt@gmail.com>
2021-09-09 23:36:41 -0400

    add information to about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -5,7 +5,21 @@ class About extends React.Component{
  render(){
      return (
        
-        <div>About!</div> 
+        <div>
+            <h1>About Deyonna Roberts!</h1>
+            <br></br>
+            <ul>
+                <li>Notary for over 7 years ( MD)</li>
+                <li>Licensed Title Producer (MD- TIPIC)</li>
+                <li>Licensed Title Producer (MD- TIPIC)</li>
+                <li>5 years experience notarizing banking, real estate and property management documents.</li>
+                <li>Completed over 200 signings</li> 
+                <li>E&O insurance of 50,00</li>
+                <li>Background Check passed</li>
+                <li>Local Black Woman owned business</li>
+            </ul>
+
+        </div> 
     
         )
     }
