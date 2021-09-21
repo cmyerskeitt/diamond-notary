c7f76699770c52af1af9f3edb46d1bd5e13ab779
github username <cmyerskeitt@gmail.com>
2021-09-20 23:32:39 -0400

    Begin creating price list

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,7 +5,25 @@ class Home extends React.Component{
  render(){
      return (
        
-        <div>Welcome to Diamond Notary!</div> 
+        <div>Welcome to Diamond Notary!
+             <table>
+                <thead>
+                    <tr>
+                        <th>Service</th>
+                        <th>Amount</th>
+                    </tr>
+                    <tbody>
+                        <tr>
+                            <td> Notarial Act(per stamp)</td><br></br>
+                            <td>Base Travel Fee</td>
+
+                        </tr>
+                    </tbody>
+                </thead>
+            </table>
+        </div> 
+       
+
     
         )
     }
