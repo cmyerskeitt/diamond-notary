f4dd7f9687ef1562d7448eb4ed2aa9e9a3b33a34
github username <cmyerskeitt@gmail.com>
2021-09-21 21:07:51 -0400

    Install react-table + build price list

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,32 +1,11 @@
 import React from "react"
+import Pricelist from "../components/Pricelist";
 
-class Home extends React.Component{
-  
- render(){
-     return (
-       
-        <div>Welcome to Diamond Notary!
-             <table>
-                <thead>
-                    <tr>
-                        <th>Service</th>
-                        <th>Amount</th>
-                    </tr>
-                    <tbody>
-                        <tr>
-                            <td> Notarial Act(per stamp)</td><br></br>
-                            <td>Base Travel Fee</td>
-
-                        </tr>
-                    </tbody>
-                </thead>
-            </table>
-        </div> 
-       
-
+function Home(){
     
-        )
-    }
+     return (
+        <Pricelist />    
+  );
 }
 
 
