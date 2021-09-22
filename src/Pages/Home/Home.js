6e03134cdcd71fc239f0cbaf3e81aa800394430d
github username <cmyerskeitt@gmail.com>
2021-09-21 23:00:52 -0400

    home- title and image styling

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,15 @@
 import React from "react"
 import Pricelist from "./Pricelist";
 import Headshot from './Headshot1.jpg'
+import './Home.css'
 
 function Home(){
     
      return (
        <>
+        <h1 className='welcome'> Welcome to Diamond Notary LLC.</h1>
         <img src={Headshot} className='headshot' alt='Headshot'/>
-        <Pricelist />  
+        <Pricelist/>  
       </> 
   );
 }
