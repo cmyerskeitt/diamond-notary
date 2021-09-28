70fc63997f88cec1fd123b958de1e1c6f73f03d0
github username <cmyerskeitt@gmail.com>
2021-09-27 22:52:01 -0400

    comment out headshot, insert logo, change background colors to grey

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,14 +4,16 @@ import Headshot from './Headshot1.jpg'
 import NSA_Badge from './nsa_member_badge.png'
 import NSA_Trained_Badge from './nsa-trained-badge.png'
 import './Home.css'
+import Logo from './DiamondNotary.png'
 
 function Home(){
     
      return (
        <>
-        <h1 className='welcome'> Welcome to Diamond Notary LLC.</h1>
+        <h1 className='welcome'> Welcome to Diamond Notary DMV, LLC.</h1>
         <img src={NSA_Badge} className='notaryBadge' alt='NSA Badge'/>
-        <img src={Headshot} className='headshot' alt='Headshot'/>
+        <img src={Logo} className='logo' alt='Logo'/>
+        {/* <img src={Headshot} className='headshot' alt='Headshot'/> */}
         <img src={NSA_Trained_Badge} className='trainedBadge' alt='NSA Trained Badge'/>
         <br></br>
         <Pricelist/>  
