63ddb3aed4d5aaabe45e003c6e65a84693b83a43
github username <cmyerskeitt@gmail.com>
2021-10-21 18:52:13 -0400

    restyle page to fit on mac size screen

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -9,7 +9,7 @@ function Home(){
      return (
        <>
         <h1 className='welcome'> Welcome to Diamond Notary DMV, LLC.</h1>
-        <h2 className='info'>Notary and Loan Signing agent with over 10 years experience notarizing accounting, real estate and property management documents. My hopes is to showcase my reliability and attention to detail to make you a return and happy customer. Call me today to schedule your signing</h2>
+        <h2 className='info'> Notary and Loan Signing agent with over 10 years experience notarizing accounting, real estate and property management documents. My hopes are to showcase my reliability and attention to detail to make you a happy returning customer. <br/> <em> Call me today to schedule your signing!</em></h2>
         <img src={NSA_Badge} className='notaryBadge' alt='NSA Badge'/>
         <img src={Logo} className='logo' alt='Logo'/>
         <img src={NSA_Trained_Badge} className='trainedBadge' alt='NSA Trained Badge'/>
