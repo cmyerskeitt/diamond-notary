833a51544632903c65601e6637e1eb006639f939
github username <cmyerskeitt@gmail.com>
2021-11-15 00:38:17 -0500

    update bio bullets

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -8,21 +8,20 @@ class About extends React.Component{
      return (
        
         <div>
-            <h1 className="welcome">About Deyonna Roberts!</h1>
+            <h1 className="welcome"> Meet Your Notary Specialist</h1>
             <img src={Headshot} className='headshot' alt='Headshot'/>
             <br></br>
             <section className='credentials'> 
-                <h2> <center> Meet Your Notary Specialist</center></h2>
+                <h2> <center> Deyonna Roberts, CEO</center></h2>
                 <br></br>
                 <ul>
-                    <li>Notary for over 7 years ( MD)</li>
+                    <li>Notary for over 7 years (MD)</li>
                     <li>Licensed Title Producer (MD- TIPIC)</li>
-                    <li>Licensed Title Producer (MD- TIPIC)</li>
-                    <li>5 years experience notarizing banking, real estate and property management documents.</li>
-                    <li>Completed over 200 signings</li> 
-                    <li>E&O insurance of 50,000</li>
-                    <li>Background Check passed</li>
-                    <li>Local Black Woman owned business</li>
+                    <li>Experience notarizing banking, real estate and property management documents</li>
+                    <li>Successfully completed over 200 signings</li> 
+                    <li>Insured: E&O of $50,000</li>
+                    <li>Completely Background Screened</li>
+                    <li>Local, Black Woman Owned Business</li>
                 </ul>
             </section>
         </div> 
