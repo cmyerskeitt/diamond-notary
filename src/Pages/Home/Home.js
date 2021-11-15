69f33d1ff1b49d7ed5cea2eb83344b69d2a7c60b
github username <cmyerskeitt@gmail.com>
2021-11-15 00:23:18 -0500

    add phone number and css styling

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -9,11 +9,18 @@ function Home(){
      return (
        <>
         <h1 className='welcome'> Welcome to Diamond Notary DMV, LLC.</h1>
-        <h2 className='info'> Notary and Loan Signing agent with over 10 years experience notarizing accounting, real estate and property management documents. My hopes are to showcase my reliability and attention to detail to make you a happy returning customer. <br/> <em> Call me today to schedule your signing!</em></h2>
-        <h3 className='hours'> <em> Hours of Operation </em> <br></br>
+        <h2 className='info'> Notary and Loan Signing agent with over 10 years experience notarizing accounting, real estate and property management documents. My hopes are to showcase my reliability and attention to detail to make you a happy returning customer. 
+        <br/> 
+        <br/>
+        <label className="info1">Call Me Today to Schedule Your Signing!
+        <br/>
+        (240)-424-9577
+        </label>
+        </h2>
+        <h2 className='hours'> Hours of Operation <br></br>
           Monday-Friday: 3pm - 9pm <br></br>
           Saturday: 9 am - 9pm <br></br>
-          Sunday: 9 am - 9 pm </h3>
+          Sunday: 9 am - 9 pm </h2>
         <img src={NSA_Badge} className='notaryBadge' alt='NSA Badge'/>
         <img src={Logo} className='logo' alt='Logo'/>
         <img src={NSA_Trained_Badge} className='trainedBadge' alt='NSA Trained Badge'/>
