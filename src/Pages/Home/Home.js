50fb7154179086b358638a46540e5af430f45e60
github username <cmyerskeitt@gmail.com>
2021-11-14 23:47:00 -0500

    add hours of aperation to home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -10,6 +10,10 @@ function Home(){
        <>
         <h1 className='welcome'> Welcome to Diamond Notary DMV, LLC.</h1>
         <h2 className='info'> Notary and Loan Signing agent with over 10 years experience notarizing accounting, real estate and property management documents. My hopes are to showcase my reliability and attention to detail to make you a happy returning customer. <br/> <em> Call me today to schedule your signing!</em></h2>
+        <h3 className='hours'> <em> Hours of Operation </em> <br></br>
+          Monday-Friday: 3pm - 9pm <br></br>
+          Saturday: 9 am - 9pm <br></br>
+          Sunday: 9 am - 9 pm </h3>
         <img src={NSA_Badge} className='notaryBadge' alt='NSA Badge'/>
         <img src={Logo} className='logo' alt='Logo'/>
         <img src={NSA_Trained_Badge} className='trainedBadge' alt='NSA Trained Badge'/>
